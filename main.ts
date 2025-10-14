8708c51921178f3a4b6569dbce69a15f6386addf
Xander-Henderson <xander.henderson@stu.ocsb.ca>
2025-10-13 20:00:57 -0400

    shows light level

--- main.ts
+++ main.ts
@@ -1,8 +1,52 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Alexander
+ * Created on: Oct 2025
+ * This program shows the light level on neopixels.
 */
 
-basic.showString('Hello, World!')
+// variables
+let lightLevel: number = 0
+let xandersNeopixelStrip: neopixel.Strip = null
+
+// setup
+basic.clearScreen()
+xandersNeopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+xandersNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+xandersNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+xandersNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+xandersNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+xandersNeopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+// 
+input.lightLevel()
+if (51 >= lightLevel) {
+    xandersNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+}
+
+input.lightLevel()
+if (52 < lightLevel) {
+    xandersNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+}
+
+input.lightLevel()
+if (104 < lightLevel) {
+    xandersNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+    xandersNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+}
+
+input.lightLevel()
+if (156 < lightLevel) {
+    xandersNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+    xandersNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+    xandersNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
+}
+
+input.lightLevel()
+if (208 < lightLevel) {
+    xandersNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+    xandersNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+    xandersNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
+    xandersNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
+}
